2e809100f04fd0cae71ea58da5aedcf447c59c58
vkedrov <vkedrov@mail.ru>
2023-04-12 16:08:41 +0300

    Изменены отступы в классе

--- sandbox/src/main/java/ru/stda/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stda/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,7 @@ package ru.stda.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-     System.out.println("Hello, world!");
+    System.out.println("Hello, world!");
   }
 
 }
